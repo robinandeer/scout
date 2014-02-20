1e74c2bab27596872e8a26d8884ac74c20cc407b
Robin Andeer <robin.andeer@gmail.com>
2014-02-20 11:31:50 +0100

    Further fixes to make debugging/deploying easier

--- runserver.py
+++ runserver.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
   if DEBUG:
     # Run in development mode
-    app.run('localhost', port=5000)
+    app.run('0.0.0.0', port=8081)
 
   else:
     # Run in production mode
