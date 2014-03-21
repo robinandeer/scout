55faac09c581356c80f357cc8664e1bf26a5723f
Robin Andeer <robin.andeer@gmail.com>
2014-03-21 12:59:40 +0100

    Adds log handler to write log to a file Reorders variant list columns

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import logging
+from logging.handlers import RotatingFileHandler
+
 from scout import app
 from scout.settings import DEBUG
 from scout.core import ctx
@@ -12,5 +16,12 @@ if __name__ == '__main__':
     app.run('0.0.0.0', port=8081)
 
   else:
+    # Set up logging to a file
+    handler = RotatingFileHandler(
+      'scout-server.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.INFO)
+    log.addHandler(handler)
+
     # Run in production mode
     app.run('0.0.0.0', port=8081, ssl_context=ctx)
